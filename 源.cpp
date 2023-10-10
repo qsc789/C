4056703d10a60487806656eef09d94ef6fa991a1
qsc789 <141476924+qsc789@users.noreply.github.com>
2023-10-10 20:52:35 +0800

    Add files via upload

---
+++ 源.cpp
@@ -0,0 +1,19 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+void Add(int *p)
+{
+	(*p)++;
+}
+int main()
+{
+	int num=0;
+	Add(&num);
+	printf("%d\n", num);
+	Add(&num);
+	printf("%d\n", num);
+
+
+	return 0;
+}
+
+
