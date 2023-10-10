172ed85802c9a0e59098e5533cdd7947650abecd
qsc789 <141476924+qsc789@users.noreply.github.com>
2023-10-10 21:13:12 +0800

    Add files via upload

---
+++ 源.cpp
@@ -0,0 +1,14 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+	
+	int len = strlen("abcdef");
+	printf("%d\n", len);
+	printf("%d\n", strlen("abcdef"));
+	return 0;
+}
+
+
