1da2b7d1e9aa8ff9b730d29c77352d0e88a51185
qsc789 <1279495882@qq.com>
2024-02-29 11:05:50 +0800

    set.insert

---
+++ C++/STL/set/set,insert.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<set>
+using namespace std;
+int main()
+{
+	set<int> s;
+	s.insert(10);
+	s.insert(30);
+	s.insert(20);
+	s.insert(40);
+	for(set<int>::iterator it=s.begin();it!=s.end();it++)
+	{
+	   cout<<*it<<endl;
+	}
+	
+	set<int>s1;
+	s1=s;
+	s1.insert(20);//不允许重复 
+	for(set<int>::iterator it=s.begin();it!=s.end();it++)
+	{
+	   cout<<*it<<endl;
+	}
+	return 0;
+ } 
